2dc180cd42d1dbd086604dde59856c1c1cf235da
Mohamed B <mohamed@jellyfish-commuting.com>
2020-03-26 15:20:41 +0100

    Add .gitignore

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const EARTH_RADIUS = 6371008; // Mean earth radius in meter see https://en.wikip
 // Haversine formula
 // https://en.wikipedia.org/wiki/Haversine_formula
 //-------
-module.exports = function (pta, ptb) {
+module.exports = function (a, b) {
   // Convert in degree
-  const a = pta.map(v => v * DEG_TO_RAD);
-  const b = ptb.map(v => v * DEG_TO_RAD);
+  const [lon1, lat1] = a.map(v => v * DEG_TO_RAD);
+  const [lon2, lat2] = b.map(v => v * DEG_TO_RAD);
 
   // Haversine formula found in wikipedia page
   return 2 * EARTH_RADIUS * Math.asin(
     Math.sqrt(
-      Math.sin((b[1] - a[1]) * 0.5) ** 2
-      + Math.cos(a[1]) * Math.cos(b[1]) * Math.sin((b[0] - a[0]) * 0.5) ** 2
+      Math.sin((lat2 - lat1) * 0.5) ** 2
+      + Math.cos(lat1) * Math.cos(lat2) * Math.sin((lon2 - lon1) * 0.5) ** 2
     )
   );
 };

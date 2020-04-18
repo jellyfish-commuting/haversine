94e4a897ca106e95cd0de190682d3475b76667b8
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-18 23:37:06 +0200

    Add unit param

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const DEG_TO_RAD = Math.PI / 180;
 const EARTH_RADIUS = 6371008; // Mean earth radius in meter see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
+const units = {
+  km: 0.001,
+  m: 1,
+  mi: 0.000621371192,
+  nmi: 0.000539956803,
+  ft: 3.280839895013,
+  in: 39.370078740158,
+};
 
 //-------
 // Haversine formula
 // https://en.wikipedia.org/wiki/Haversine_formula
 //-------
-module.exports = function (a, b) {
+module.exports = function (a, b, unit='m') {
   // Convert in rads
   const [lon1, lat1] = Array.isArray(a) ? [a[0] * DEG_TO_RAD, a[1] * DEG_TO_RAD] : [a.longitude * DEG_TO_RAD, a.latitude * DEG_TO_RAD];
   const [lon2, lat2] = Array.isArray(b) ? [b[0] * DEG_TO_RAD, b[1] * DEG_TO_RAD] : [b.longitude * DEG_TO_RAD, b.latitude * DEG_TO_RAD];
 
   // Haversine formula found in wikipedia page
-  return 2 * EARTH_RADIUS * Math.asin(
+  return 2 * EARTH_RADIUS * units[unit] * Math.asin(
     Math.sqrt(
       Math.sin((lat2 - lat1) / 2) ** 2
       + Math.cos(lat1) * Math.cos(lat2) * Math.sin((lon2 - lon1) / 2) ** 2
--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const haversine = require('./index');
 
 // Start tests ...
 describe('Haversine', () => {
-  it('expect distance between Bejaia and Tizi Ouzou equal around 83Km', () => {
+  it('expect distance in meter between Bejaia and Tizi Ouzou equal around 83Km', () => {
     expect(haversine(
       [4.8668945,36.7699898],
       [3.9354349,36.6988394],
@@ -10,11 +10,101 @@ describe('Haversine', () => {
       .toBe(83381.53382511878);
   });
 
-  it('expect distance between Bejaia and Tizi Ouzou equal around 83Km', () => {
+  it('expect distance in meter between Bejaia and Tizi Ouzou equal around 83Km', () => {
     expect(haversine(
       { longitude: 4.8668945, latitude: 36.7699898 },
       { longitude: 3.9354349, latitude: 36.6988394 },
     ))
-    .toBe(83381.53382511878);
+      .toBe(83381.53382511878);
+  });
+
+  it('expect distance in Km between Bejaia and Tizi Ouzou equal around 83Km', () => {
+    expect(haversine(
+      [4.8668945,36.7699898],
+      [3.9354349,36.6988394],
+      'km',
+    ))
+      .toBe(83.38153382511877);
+  });
+
+  it('expect distance in Km between Bejaia and Tizi Ouzou equal around 83Km', () => {
+    expect(haversine(
+      { longitude: 4.8668945, latitude: 36.7699898 },
+      { longitude: 3.9354349, latitude: 36.6988394 },
+      'km',
+    ))
+      .toBe(83.38153382511877);
+  });
+
+  it('expect distance in miles between Bejaia and Tizi Ouzou equal around 51mi', () => {
+    expect(haversine(
+      [4.8668945,36.7699898],
+      [3.9354349,36.6988394],
+      'mi',
+    ))
+      .toBe(51.81088306370237);
+  });
+
+  it('expect distance in miles between Bejaia and Tizi Ouzou equal around 51mi', () => {
+    expect(haversine(
+      { longitude: 4.8668945, latitude: 36.7699898 },
+      { longitude: 3.9354349, latitude: 36.6988394 },
+      'mi',
+    ))
+      .toBe(51.81088306370237);
+  });
+
+  it('expect distance in nautical miles between Bejaia and Tizi Ouzou equal around 45nmi', () => {
+    expect(haversine(
+      [4.8668945,36.7699898],
+      [3.9354349,36.6988394],
+      'nmi',
+    ))
+      .toBe(45.02242643344749);
+  });
+
+  it('expect distance in nautical miles between Bejaia and Tizi Ouzou equal around 45nmi', () => {
+    expect(haversine(
+      { longitude: 4.8668945, latitude: 36.7699898 },
+      { longitude: 3.9354349, latitude: 36.6988394 },
+      'nmi',
+    ))
+      .toBe(45.02242643344749);
+  });
+
+  it('expect distance in feet between Bejaia and Tizi Ouzou equal around 273561ft', () => {
+    expect(haversine(
+      [4.8668945,36.7699898],
+      [3.9354349,36.6988394],
+      'ft',
+    ))
+      .toBe(273561.46268082556);
+  });
+
+  it('expect distance in feet between Bejaia and Tizi Ouzou equal around 273561ft', () => {
+    expect(haversine(
+      { longitude: 4.8668945, latitude: 36.7699898 },
+      { longitude: 3.9354349, latitude: 36.6988394 },
+      'ft',
+    ))
+    .toBe(273561.46268082556);
+  });
+
+  it('expect distance in inch between Bejaia and Tizi Ouzou equal around 3282737in', () => {
+    expect(haversine(
+      [4.8668945,36.7699898],
+      [3.9354349,36.6988394],
+      'in',
+    ))
+      .toBe(3282737.5521700736);
+  });
+
+  it('expect distance in inch between Bejaia and Tizi Ouzou equal around 3282737in', () => {
+    expect(haversine(
+      { longitude: 4.8668945, latitude: 36.7699898 },
+      { longitude: 3.9354349, latitude: 36.6988394 },
+      'in',
+    ))
+      .toBe(3282737.5521700736);
   });
 });

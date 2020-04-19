7921b9812f6ffb08215cf9bff4a80624a0ac13cf
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-19 11:33:48 +0200

    Capitalize constant names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// Constants
 const DEG_TO_RAD = Math.PI / 180;
 const EARTH_RADIUS = 6371008; // Mean earth radius in meter see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
-const units = {
+const UNITS = {
   km: 0.001,
   m: 1,
   mi: 0.000621371192,
@@ -18,8 +19,8 @@ module.exports = function (a, b, unit='m') {
   const [lon1, lat1] = Array.isArray(a) ? [a[0] * DEG_TO_RAD, a[1] * DEG_TO_RAD] : [a.longitude * DEG_TO_RAD, a.latitude * DEG_TO_RAD];
   const [lon2, lat2] = Array.isArray(b) ? [b[0] * DEG_TO_RAD, b[1] * DEG_TO_RAD] : [b.longitude * DEG_TO_RAD, b.latitude * DEG_TO_RAD];
 
-  // Haversine formula found in wikipedia page
-  return 2 * EARTH_RADIUS * units[unit] * Math.asin(
+  // Apply formula
+  return 2 * EARTH_RADIUS * UNITS[unit] * Math.asin(
     Math.sqrt(
       Math.sin((lat2 - lat1) / 2) ** 2
       + Math.cos(lat1) * Math.cos(lat2) * Math.sin((lon2 - lon1) / 2) ** 2
